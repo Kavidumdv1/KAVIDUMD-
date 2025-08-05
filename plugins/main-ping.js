4e49ebe2ba982c2dd75afa20c1416cd4fa27f51a
Kavidumdv1 <Kavidumd2025@outlook.com>
2025-08-05 21:03:51 +0530

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363400240662312@newsletter',
-                    newsletterName: "𝙳𝙰𝚁𝙺-𝙺𝙽𝙸𝙶𝙷𝚃-𝚇𝙼𝙳",
+                    newsletterJid: '120363422383085384@newsletter',
+                    newsletterName: "𝙺𝙰𝚅𝙸𝙳𝚄-𝙼𝙳",
                     serverMessageId: 143
                 }
             }
